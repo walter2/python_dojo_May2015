9921523ee2a66f2847b1311024ec7c05d7c301a7
Paul Egan <paulegan@mail.com>
2015-05-07 21:08:42 +0100

    Read source_file from args

--- code_parser.py
+++ code_parser.py
@@ -1,3 +1,4 @@
+import sys
 import keyword
 import requests
 
@@ -8,6 +9,7 @@ FLIKR_IMAGE_KEY = 'url_t'
 CODE_TEMPLATE = '''
     <div>
         <pre>{code}</pre>
+        <strong>{word}</strong>
         <img src="{image_url}"/>
     </div>
 '''
@@ -18,7 +20,6 @@ def clean_code(code):
     for line_number, line in code:
         split_code = line.split(' ')
         for word in split_code:
-
             if word.isalpha() and len(word) >= 3 and word not in keyword.kwlist:
                 cleaned_code.append((line, word))
     return set(cleaned_code)
@@ -28,8 +29,8 @@ def parse_code(_file):
     with open(_file) as f:
         code = enumerate(f.readlines())
     cleaned_code = clean_code(code)
-    for keyword in cleaned_code:
-        yield keyword
+    for line, word in cleaned_code:
+        yield line, word
 
 
 def get_image_url(keyword):
@@ -48,7 +49,17 @@ def get_image_url(keyword):
 
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        source_file = sys.argv[1]
+    else:
+        source_file = __file__
+
     with open('temp.html', 'w') as f:
-        for code, word in parse_code('code_parser.py'):
+        for code, word in parse_code(source_file):
+            print(word)
             image_url = get_image_url(word)
-            f.write(CODE_TEMPLATE.format(code=code, image_url=image_url))
+            f.write(CODE_TEMPLATE.format(
+                code=code,
+                image_url=image_url,
+                word=word,
+            ))
